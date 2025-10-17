086ebedcd65378af5c198b6189533da8b4cece70
Illia Tarabarov <tarabarov.illia@gmail.com>
2025-10-17 16:49:02 +0200

    Illia-Tarabarov-card

---
+++ script.js
@@ -0,0 +1,21 @@
+// --------------------------------------------------------------------------
+document.querySelector(".github").addEventListener("click", function () {
+  window.location.href = "https://github.com/tarabarovillia-svg";
+});
+document.querySelector(".instagram").addEventListener("click", function () {
+  window.location.href = "https://www.instagram.com/alkatpa3/";
+});
+document.querySelector(".threads").addEventListener("click", function () {
+  window.location.href = "https://github.com/tarabarovillia-svg/Illia-Tarabarov-card.git";
+});
+document.querySelector(".linkedin").addEventListener("click", function () {
+  window.location.href =
+    "https://www.linkedin.com/in/illia-tarabarov-1bb4a6358/";
+});
+document.querySelector(".email").addEventListener("click", function () {
+  window.location.href = "mailto:tarabarov.illia@gmail.com";
+});
+document.querySelector(".tel").addEventListener("click", function () {
+  window.location.href = "tel:+420722276279";
+});
+// --------------------------------------------------------------------------
